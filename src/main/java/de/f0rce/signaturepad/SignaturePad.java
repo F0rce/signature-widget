3aa3424357efb83f64682d23bfdc36bf2c173b69
David Dodlek <david@dodlek.com>
2022-01-20 11:01:04 +0100

    fixed rgba not working
    fixes #201

--- src/main/java/de/f0rce/signaturepad/SignaturePad.java
+++ src/main/java/de/f0rce/signaturepad/SignaturePad.java
@@ -11,12 +11,12 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 
 /**
  * Main class with all methods.
- * 
+ *
  * @author David "F0rce" Dodlek
  */
 @Tag("lit-signature-pad")
 @JsModule("./@f0rce/signature-widget.js")
-@NpmPackage(value = "signature_pad", version = "3.0.0-beta.4")
+@NpmPackage(value = "signature_pad", version = "3.0.0-beta.3")
 public class SignaturePad extends Component implements HasSize {
 
 	private double dotSize;
@@ -38,8 +38,8 @@ public class SignaturePad extends Component implements HasSize {
 	 */
 	public SignaturePad() {
 		super.addListener(ImageEncode.class, this::updateImage);
-		setHeight("100px");
-		setWidth("300px");
+		this.setHeight("100px");
+		this.setWidth("300px");
 	}
 
 	// Used to sync the private variables (called after the frontend event has been
@@ -54,243 +54,243 @@ public class SignaturePad extends Component implements HasSize {
 	 * Clears the widget.
 	 */
 	public void clear() {
-		getElement().callJsFunction("clear");
+		this.getElement().callJsFunction("clear");
 	}
 
 	/**
 	 * Sets the radius of a single dot.
-	 * 
+	 *
 	 * @param dotSize int
 	 */
 	public void setDotSize(int dotSize) {
 		double dotSizeD = Double.valueOf(dotSize);
-		getElement().setProperty("dotSize", dotSizeD);
+		this.getElement().setProperty("dotSize", dotSizeD);
 		this.dotSize = dotSizeD;
 	}
 
 	/**
 	 * Sets the radius of a single dot.
-	 * 
+	 *
 	 * @param dotSize double
 	 */
 	public void setDotSize(double dotSize) {
-		getElement().setProperty("dotSize", dotSize);
+		this.getElement().setProperty("dotSize", dotSize);
 		this.dotSize = dotSize;
 	}
 
 	/**
 	 * Returns the current set radius of a single dot.
-	 * 
+	 *
 	 * @return double
 	 */
 	public double getDotSize() {
-		return dotSize;
+		return this.dotSize;
 	}
 
 	/**
 	 * Sets the minimum width of a line. Defaults to 0.5.
-	 * 
+	 *
 	 * @param lineMinWidth int
 	 */
 	public void setLineMinWidth(int lineMinWidth) {
 		double lineMinWidthD = Double.valueOf(lineMinWidth);
-		getElement().setProperty("minWidth", lineMinWidthD);
+		this.getElement().setProperty("minWidth", lineMinWidthD);
 		this.lineMinWidth = lineMinWidthD;
 	}
 
 	/**
 	 * Sets the minimum width of a line. Defaults to 0.5.
-	 * 
+	 *
 	 * @param lineMinWidth double
 	 */
 	public void setLineMinWidth(double lineMinWidth) {
-		getElement().setProperty("minWidth", lineMinWidth);
+		this.getElement().setProperty("minWidth", lineMinWidth);
 		this.lineMinWidth = lineMinWidth;
 	}
 
 	/**
 	 * Returns the current set minimum width of a line.
-	 * 
+	 *
 	 * @return double
 	 */
 	public double getLineMinWidth() {
-		return lineMinWidth;
+		return this.lineMinWidth;
 	}
 
 	/**
 	 * Sets the maximum width of a line. Defaults to 2.5.
-	 * 
+	 *
 	 * @param lineMaxWidth int
 	 */
 	public void setLineMaxWidth(int lineMaxWidth) {
 		double lineMaxWidthD = Double.valueOf(lineMaxWidth);
-		getElement().setProperty("maxWidth", lineMaxWidthD);
+		this.getElement().setProperty("maxWidth", lineMaxWidthD);
 		this.lineMaxWidth = lineMaxWidthD;
 	}
 
 	/**
 	 * Sets the maximum width of a line. Defaults to 2.5.
-	 * 
+	 *
 	 * @param lineMaxWidth double
 	 */
 	public void setLineMaxWidth(double lineMaxWidth) {
-		getElement().setProperty("maxWidth", lineMaxWidth);
+		this.getElement().setProperty("maxWidth", lineMaxWidth);
 		this.lineMaxWidth = lineMaxWidth;
 	}
 
 	/**
 	 * Returns the current set maximum width of a line.
-	 * 
+	 *
 	 * @return double
 	 */
 	public double getLineMaxWidth() {
-		return lineMaxWidth;
+		return this.lineMaxWidth;
 	}
 
 	/**
 	 * Sets the throttle, which will draw the next point at most once per every x
 	 * milliseconds. Set it to 0 to turn off throttling. Defaults to 16.
-	 * 
+	 *
 	 * @param throttle int
 	 */
 	public void setThrottle(int throttle) {
-		getElement().setProperty("throttle", throttle);
+		this.getElement().setProperty("throttle", throttle);
 		this.throttle = throttle;
 	}
 
 	/**
 	 * Returns the current set throttle.
-	 * 
+	 *
 	 * @return int
 	 */
 	public int getThrottle() {
-		return throttle;
+		return this.throttle;
 	}
 
 	/**
 	 * Sets the minDistance, which adds the next point only if the previous one is
 	 * farther than x pixels. Defaults to 5.
-	 * 
+	 *
 	 * @param minDistance int
 	 */
 	public void setMinDistance(int minDistance) {
-		getElement().setProperty("minDistance", minDistance);
+		this.getElement().setProperty("minDistance", minDistance);
 		this.minDistance = minDistance;
 	}
 
 	/**
 	 * Returns the current set min distance.
-	 * 
+	 *
 	 * @return int
 	 */
 	public int getMinDistance() {
-		return minDistance;
+		return this.minDistance;
 	}
 
 	/**
 	 * Sets the background color in RGB format.
-	 * 
+	 *
 	 * @param red   int
 	 * @param green int
 	 * @param blue  int
 	 */
 	public void setBackgroundColor(int red, int green, int blue) {
 		String rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
-		getElement().setProperty("backgroundColor", rgb);
-		backgroundColor = rgb;
+		this.getElement().setProperty("backgroundColor", rgb);
+		this.backgroundColor = rgb;
 	}
 
 	/**
 	 * Sets the background color in RGBA format.
-	 * 
+	 *
 	 * @param red   int
 	 * @param green int
 	 * @param blue  int
 	 * @param alpha int
 	 */
 	public void setBackgroundColor(int red, int green, int blue, int alpha) {
-		String rgba = "rgb(" + red + ", " + green + ", " + blue + ", " + alpha + ")";
-		getElement().setProperty("backgroundColor", rgba);
-		backgroundColor = rgba;
+		String rgba = "rgba(" + red + ", " + green + ", " + blue + ", " + alpha + ")";
+		this.getElement().setProperty("backgroundColor", rgba);
+		this.backgroundColor = rgba;
 	}
 
 	/**
 	 * Sets the background color in hex format.
-	 * 
+	 *
 	 * @param hex {@link String}
 	 */
 	public void setBackgroundColor(String hex) {
-		getElement().setProperty("backgroundColor", hex);
-		backgroundColor = hex;
+		this.getElement().setProperty("backgroundColor", hex);
+		this.backgroundColor = hex;
 	}
 
 	/**
 	 * Returns the current set background color.
-	 * 
+	 *
 	 * @return {@link String}
 	 */
 	public String getBackgroundColor() {
-		return backgroundColor;
+		return this.backgroundColor;
 	}
 
 	/**
 	 * Sets the pen color in RGB format.
-	 * 
+	 *
 	 * @param red   int
 	 * @param green int
 	 * @param blue  int
 	 */
 	public void setPenColor(int red, int green, int blue) {
 		String rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
-		getElement().setProperty("penColor", rgb);
-		penColor = rgb;
+		this.getElement().setProperty("penColor", rgb);
+		this.penColor = rgb;
 	}
 
 	/**
 	 * Sets the pen color in hex format.
-	 * 
+	 *
 	 * @param hex {@link String}
 	 */
 	public void setPenColor(String hex) {
-		getElement().setProperty("penColor", hex);
-		penColor = hex;
+		this.getElement().setProperty("penColor", hex);
+		this.penColor = hex;
 	}
 
 	/**
 	 * Returns the current set pen color.
-	 * 
+	 *
 	 * @return {@link String}
 	 */
 	public String getPenColor() {
-		return penColor;
+		return this.penColor;
 	}
 
 	/**
 	 * Sets the velocityFilterWeight, which is used to modify new velocity based on
 	 * the previous velocity. Defaults to 0.7
-	 * 
+	 *
 	 * @param velocityFilterWeight int
 	 */
 	public void setVelocityFilterWeight(int velocityFilterWeight) {
 		double velocityFilterWeightD = Double.valueOf(velocityFilterWeight);
-		getElement().getProperty("velocityFilterWeight", velocityFilterWeightD);
+		this.getElement().getProperty("velocityFilterWeight", velocityFilterWeightD);
 		this.velocityFilterWeight = velocityFilterWeightD;
 	}
 
 	/**
 	 * Sets the velocityFilterWeight, which is used to modify new velocity based on
 	 * the previous velocity. Defaults to 0.7
-	 * 
+	 *
 	 * @param velocityFilterWeight double
 	 */
 	public void setVelocityFilterWeight(double velocityFilterWeight) {
-		getElement().getProperty("velocityFilterWeight", velocityFilterWeight);
+		this.getElement().getProperty("velocityFilterWeight", velocityFilterWeight);
 		this.velocityFilterWeight = velocityFilterWeight;
 	}
 
 	/**
 	 * Returns the current set velocityFilterWeight.
-	 * 
+	 *
 	 * @return double
 	 */
 	public double getVelocityFilterWeight() {
@@ -299,7 +299,7 @@ public class SignaturePad extends Component implements HasSize {
 
 	/**
 	 * Returns the current shown image in URI format.
-	 * 
+	 *
 	 * @return {@link String}
 	 */
 	public String getImageURI() {
@@ -308,33 +308,33 @@ public class SignaturePad extends Component implements HasSize {
 
 	/**
 	 * Returns the current shown image as Base64 decoded byte array.
-	 * 
+	 *
 	 * @return byte[]
 	 */
 	public byte[] getImageBase64() {
-		if (imageUri.equals("")) {
+		if (this.imageUri.equals("")) {
 			return null;
 		}
-		String split = imageUri.split(",")[1];
+		String split = this.imageUri.split(",")[1];
 		return Base64.getDecoder().decode(split.getBytes(StandardCharsets.UTF_8));
 	}
 
 	/**
 	 * Sets the MIME-Type for the image encoder. Has to start with "image/"!
-	 * 
+	 *
 	 * @param type {@link String}
 	 */
 	public void setType(String type) {
 		if (!type.contains("image/")) {
 			type = "image/png";
 		}
-		getElement().setProperty("type", type);
+		this.getElement().setProperty("type", type);
 		this.type = type;
 	}
 
 	/**
 	 * Returns the current set MIME-Type. Defaults to image/png.
-	 * 
+	 *
 	 * @return {@link String}
 	 */
 	public String getType() {
@@ -343,17 +343,17 @@ public class SignaturePad extends Component implements HasSize {
 
 	/**
 	 * Sets the widget read only.
-	 * 
+	 *
 	 * @param readOnly boolean
 	 */
 	public void setReadOnly(boolean readOnly) {
-		getElement().setProperty("readOnly", readOnly);
+		this.getElement().setProperty("readOnly", readOnly);
 		this.readOnly = readOnly;
 	}
 
 	/**
 	 * Returns if the widget is set to read only.
-	 * 
+	 *
 	 * @return boolean
 	 */
 	public boolean isReadOnly() {
@@ -362,18 +362,18 @@ public class SignaturePad extends Component implements HasSize {
 
 	/**
 	 * Sets the image of the widget in URI Format.
-	 * 
+	 *
 	 * @param uri {@link String}
 	 */
 	public void setImage(String uri) {
-		getElement().setProperty("img", uri);
+		this.getElement().setProperty("img", uri);
 		this.imageUri = uri;
 	}
 
 	/**
 	 * Sets the encoder quality. All values between 0-1 are accepted. Defaults to
 	 * 0.85.
-	 * 
+	 *
 	 * @param encoderQuality double
 	 */
 	public void setEncoderQuality(double encoderQuality) {
@@ -381,13 +381,13 @@ public class SignaturePad extends Component implements HasSize {
 		if (x > 1) {
 			x = 1.00;
 		}
-		getElement().setProperty("encoderOptions", x);
+		this.getElement().setProperty("encoderOptions", x);
 		this.encoderQuality = x;
 	}
 
 	/**
 	 * Returns the current set encoder quality.
-	 * 
+	 *
 	 * @return double
 	 */
 	public double getEncoderQuality() {
@@ -398,12 +398,12 @@ public class SignaturePad extends Component implements HasSize {
 	 * Reverts the last change you did to the canvas itself.
 	 */
 	public void undo() {
-		getElement().callJsFunction("undo");
+		this.getElement().callJsFunction("undo");
 	}
 
 	/**
 	 * Returns if the widget is empty.
-	 * 
+	 *
 	 * @return boolean
 	 */
 	public boolean isEmpty() {
@@ -414,7 +414,7 @@ public class SignaturePad extends Component implements HasSize {
 	 * Sets the background color transparent.
 	 */
 	public void setTransparentBackground() {
-		getElement().setProperty("backgroundColor", "rgb(255, 255, 255)");
+		this.getElement().setProperty("backgroundColor", "rgb(255, 255, 255)");
 	}
 
 }
