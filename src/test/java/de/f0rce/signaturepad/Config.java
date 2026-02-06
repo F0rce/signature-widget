83cb1ebd8f9b6dca851543bb8262d1f30e386b89
TatuWorkLenovo\Tatu <tatu@vaadin.com>
2026-02-06 13:07:38 +0200

    Update to Vaadin 25

---
+++ src/test/java/de/f0rce/signaturepad/Config.java
@@ -0,0 +1,10 @@
+package de.f0rce.signaturepad;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@StyleSheet(Lumo.STYLESHEET)
+public class Config implements AppShellConfigurator {
+    
+}
